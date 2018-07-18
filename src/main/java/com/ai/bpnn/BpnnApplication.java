4b12a8e12a5e18b207e97e5ebad9c633dc7499f4
hoonjo <hoonjo@linecorp.com>
2018-07-18 11:19:38 +0900

    first commit

---
+++ src/main/java/com/ai/bpnn/BpnnApplication.java
@@ -0,0 +1,12 @@
+package com.ai.bpnn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BpnnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BpnnApplication.class, args);
+	}
+}
